32ed5c1f61b3ca834d83a4146f4715e79ed4abb6
g1ddy <g1ddy@users.noreply.github.com>
2024-01-12 17:19:59 -0500

    minimum spanning tree for archetypes

--- classes/Archetype.ps1
+++ classes/Archetype.ps1
@@ -10,4 +10,27 @@ Class Archetype {
     [string]$Face
     [string]$Ear
     [string]$Color
+
+    [double] GetDistance([Archetype]$otherArchetype) {
+        $properties = $otherArchetype | Get-Member -MemberType Properties | Select-Object -ExpandProperty Name
+            | Where-Object {$_ -notin @('Name', 'Color') }
+        
+        # force to return as array
+        $properties = @($properties)
+        $totalProperties = $properties.Count
+    
+        $matchingProperties = 0
+    
+        foreach ($property in $properties) {
+            if (!$this.$property -or !$otherArchetype.$property) {
+                $matchingProperties = $matchingProperties + 0.3
+            }
+            elseif ($this.$property -eq $otherArchetype.$property) {
+                $matchingProperties++
+            }
+        }
+    
+        $matchDistance = $totalProperties - $matchingProperties
+        return $matchDistance
+    }
 }
